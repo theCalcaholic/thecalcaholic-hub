e3b948c21dc0fa3373bb4389c4d5331bb8ba7b2c
theCalcaholic <3knoeppl@informatik.uni-hamburg.de>
2014-03-25 10:43:15 +0100

    first development version of thecalcaholics hub. Created index page. Tests  still missing (have been lazy).

--- hub/main_app.py
+++ hub/main_app.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template
 import sys
 app = Flask(__name__)
 
+"""Show index page."""
 @app.route('/')
 def index():
-    return hub()
-
-@app.route('/hub/')
-def hub():
     return render_template('index.html')
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
