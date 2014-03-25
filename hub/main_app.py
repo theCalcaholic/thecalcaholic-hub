a7e0955c374ca7ba26819791173bf667ba1160b8
theCalcaholic <3knoeppl@informatik.uni-hamburg.de>
2014-03-25 11:43:08 +0100

    Added preliminary session management

--- hub/main_app.py
+++ hub/main_app.py
@@ -1,19 +1,26 @@
-from flask import Flask, render_template, session, request, redirect, url_for
+from flask import Flask, render_template, session, request, redirect, url_for, make_response
 import sys
 app = Flask(__name__)
 
-"""Show index page."""
+
 @app.route('/')
 def index():
+    """Show index page."""
     if 'logged_in' in session and session['logged_in']:
         return render_template('private/index.html')
+    elif request.cookies.get('login_attempt') == 'yes':
+        return redirect(url_for('login'))
     return render_template('public/index.html')
+
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
+    """Show login form."""
     if request.method == 'POST' and request.form['password'] == 'theMasterIsPresent':
         session['logged_in'] = True
-        return redirect(url_for('index'))
+        resp = make_response( redirect(url_for('index')) )
+        resp.set_cookie('login_attempt', "yes")
+        return resp
     return render_template('private/login.html')
 
 
